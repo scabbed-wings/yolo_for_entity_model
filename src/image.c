edc587d0202d0ba1a0a314dfba389d4f1444f024
scabbed-wings <scabbed.wings.demon@gmail.com>
2020-12-10 18:13:40 +0100

    Fixing problem on draw_detections function

--- src/image.c
+++ src/image.c
@@ -256,7 +256,7 @@ void draw_detections(image im, detection *dets, int num, float thresh, char **na
                     strcat(labelstr, names[j]);
 					strcat(labelstr, num_element);
                 }
-                printf("%s %s: %.0f%%\n", names[j], num_element dets[i].prob[j]*100);
+                printf("%s %s: %.0f%%\n", names[j], num_element, dets[i].prob[j]*100);
             }
         }
         if(class >= 0){
